e28c2c12c9af18cf9e0ee1ea0f718fe826901b1d
patrick <chodowski.patrick@gmail.com>
2025-05-14 21:45:33 +0200

    move back

--- src/lib.rs
+++ src/lib.rs
@@ -127,7 +127,7 @@ fn attach_animation_graphs(
             for anim in anims_config.anims_with_start_event.iter(){
                 let anim_clip = get_clip(player_graph.animations[*anim], graph, &mut clips);
                 info!(" [PG ANIMS] Attaching Anim Start Event to {}", anim);
-                anim_clip.add_event(anim_clip.duration(), AnimStartEvent{
+                anim_clip.add_event(0.0, AnimStartEvent{
                     anim: *anim, 
                     armature_entity: armature_entity
                 });
