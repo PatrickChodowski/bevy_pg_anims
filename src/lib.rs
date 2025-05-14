9faf8d749e99304c5fffd4e7b7e3a9f7110d5d23
patrick <chodowski.patrick@gmail.com>
2025-05-14 21:28:23 +0200

    typo

--- src/lib.rs
+++ src/lib.rs
@@ -126,7 +126,7 @@ fn attach_animation_graphs(
             // Events
             for anim in anims_config.anims_with_start_event.iter(){
                 let anim_clip = get_clip(player_graph.animations[*anim], graph, &mut clips);
-                info!(" [PG ANIMS] Attacking Anim Start Event to {}", anim);
+                info!(" [PG ANIMS] Attaching Anim Start Event to {}", anim);
                 anim_clip.add_event(anim_clip.duration(), AnimStartEvent{
                     anim: *anim, 
                     armature_entity: armature_entity
@@ -135,7 +135,7 @@ fn attach_animation_graphs(
 
             for anim in anims_config.anims_with_end_event.iter(){
                 let anim_clip = get_clip(player_graph.animations[*anim], graph, &mut clips);
-                info!(" [PG ANIMS] Attacking Anim End Event to {}", anim);
+                info!(" [PG ANIMS] Attaching Anim End Event to {}", anim);
                 anim_clip.add_event(anim_clip.duration(), AnimEndEvent{
                     anim: *anim, 
                     armature_entity: armature_entity
